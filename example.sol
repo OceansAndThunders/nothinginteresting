c3c7cb5f766eef81eb931d3a5885601990e4e6e2
OceansAndThunders <68697244+OceansAndThunders@users.noreply.github.com>
2022-02-27 20:53:48 +0100

    Update example.sol

--- example.sol
+++ example.sol
@@ -2,7 +2,7 @@ pragma solidity 0.6.10;
 
 
 
-contract example{
+contract example_contract{
  function checkTXO () public returns (string memory) {
    if (tx.origin == msg.sender) {
          return 'they are the same';
