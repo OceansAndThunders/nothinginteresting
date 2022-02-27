d57697e9cb29fc269060f380200bd0502081646b
OceansAndThunders <68697244+OceansAndThunders@users.noreply.github.com>
2022-02-27 21:11:10 +0100

    Update example.sol

--- example.sol
+++ example.sol
@@ -2,14 +2,12 @@ pragma solidity 0.6.10;
 
 
 
-contract example_contract{
- function checkTXO () public returns (bool) {
-   if (tx.origin == msg.sender) {
-         return true;
-         // do stuff
-   }
-   else {
-         return false;
-   }
- }
+contract example_contract {
+
+    function transfer(address payable receipent, uint amount) public {
+        require(tx.origin == msg.sender, "not authorized");
+
+        (bool sent, ) = receipent.call{value: amount}("");
+        require(sent, "something wrong");
+    }
 }
